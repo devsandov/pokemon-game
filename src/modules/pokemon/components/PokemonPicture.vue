656cda8cbeb050050f6fcc60474e3caa2540e942
David <davidsandoval0051@gmail.com>
2024-10-18 15:59:53 -0600

    feature: structure created

---
+++ src/modules/pokemon/components/PokemonPicture.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <img
+      src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/132.svg"
+      class="brightness-0"
+    />
+  </section>
+</template>
+
+<script setup lang="ts"></script>
+<style scoped>
+img {
+  user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  -webkit-user-drag: none;
+  -webkit-user-select: none;
+  height: 200px;
+}
+</style>
---
+++ src/modules/pokemon/pages/PokemonGame.vue
@@ -0,0 +1,18 @@
+<template>
+  <section v-if="false" class="flex flex-col justify-center items-center w-screen h-screen">
+    <h1 class="text-3xl">Wait a minute!</h1>
+    <h3 class="animate-pulse">Loading pokemons...</h3>
+  </section>
+
+  <section class="flex flex-col justify-center items-center w-screen h-screen">
+    <h1 class="m-5">Who is this pokemon?</h1>
+    <PokemonPicture />
+
+    <PokemonOptions />
+  </section>
+</template>
+
+<script setup lang="ts">
+import PokemonOptions from '../components/PokemonOptions.vue';
+import PokemonPicture from '../components/PokemonPicture.vue';
+</script>
