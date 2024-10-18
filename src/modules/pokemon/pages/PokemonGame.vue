2eba2f6ca4aa87c4dad7e98c70b0918803ec9f6d
David <davidsandoval0051@gmail.com>
2024-10-18 17:44:10 -0600

    feature: options for pokemons

--- src/modules/pokemon/pages/PokemonGame.vue
+++ src/modules/pokemon/pages/PokemonGame.vue
@@ -1,10 +1,10 @@
 <template>
-  <section v-if="false" class="flex flex-col justify-center items-center w-screen h-screen">
+  <section v-if="isLoading" class="flex flex-col justify-center items-center w-screen h-screen">
     <h1 class="text-3xl">Wait a minute!</h1>
     <h3 class="animate-pulse">Loading pokemons...</h3>
   </section>
 
-  <section class="flex flex-col justify-center items-center w-screen h-screen">
+  <section v-else class="flex flex-col justify-center items-center w-screen h-screen">
     <h1 class="m-5">Who is this pokemon?</h1>
     <PokemonPicture />
 
@@ -15,4 +15,7 @@
 <script setup lang="ts">
 import PokemonOptions from '../components/PokemonOptions.vue';
 import PokemonPicture from '../components/PokemonPicture.vue';
+import { usePokemonGame } from '../composables/usePokemonGame';
+
+const { gameStatus, isLoading } = usePokemonGame();
 </script>
