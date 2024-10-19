89c7fde98b192de6a2d1695ac4fdc1122e9a280e
David <davidsandoval0051@gmail.com>
2024-10-18 18:52:34 -0600

    feature: options configured

--- src/modules/pokemon/pages/PokemonGame.vue
+++ src/modules/pokemon/pages/PokemonGame.vue
@@ -1,5 +1,8 @@
 <template>
-  <section v-if="isLoading" class="flex flex-col justify-center items-center w-screen h-screen">
+  <section
+    v-if="isLoading || randomPokemon.id === null"
+    class="flex flex-col justify-center items-center w-screen h-screen"
+  >
     <h1 class="text-3xl">Wait a minute!</h1>
     <h3 class="animate-pulse">Loading pokemons...</h3>
   </section>
@@ -7,9 +10,12 @@
   <section v-else class="flex flex-col justify-center items-center w-screen h-screen">
     <h1 class="m-5">Who is this pokemon?</h1>
     <h2>{{ randomPokemon }}</h2>
-    <PokemonPicture />
+    <PokemonPicture
+      :pokemon-id="randomPokemon.id"
+      :show-pokemon="gameStatus !== GameStatus.Playing"
+    />
 
-    <PokemonOptions />
+    <PokemonOptions :options="options" @selected-option="onSelectedOption" />
   </section>
 </template>
 
@@ -17,6 +23,10 @@
 import PokemonOptions from '../components/PokemonOptions.vue';
 import PokemonPicture from '../components/PokemonPicture.vue';
 import { usePokemonGame } from '../composables/usePokemonGame';
+import { GameStatus } from '../interfaces';
 
-const { randomPokemon, isLoading } = usePokemonGame();
+const { randomPokemon, gameStatus, pokemonOptions: options, isLoading } = usePokemonGame();
+const onSelectedOption = (value: number) => {
+  console.log();
+};
 </script>
